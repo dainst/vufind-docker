c29e86a1ae02d4ad6433d134049ab9c588afe573
Simon Hohl <simon.hohl@dainst.org>
2019-07-01 16:19:54 +0200

    First development setup for VuFind
    See .env_template for Solr options (running locally or proxied
    Development server).

---
+++ docker-application/docker-entrypoint.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+cd /usr/local/vufind
+
+# vendor directory is created while installing dependencies using composer, if the directory is missing run VuFind's
+# installation procedure.
+if [ ! -d "vendor" ]; then
+
+    # Besides installing dependencies, set MySQL credentials correctly.
+    cp /usr/local/vufind-configs/config.ini /usr/local/vufind/local/config/vufind/config.ini
+    sed -i 's#mysql://.*#mysql://'"$MYSQL_USER_NAME"':'"$MYSQL_USER_PASSWORD"'@'"$MYSQL_HOST_NAME"'/'"$MYSQL_DATABASE_NAME"'"#g' /usr/local/vufind/local/config/vufind/config.ini
+
+    # Install dependencies using composer.
+    EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+    then
+        >&2 echo 'ERROR: Invalid installer signature'
+        rm composer-setup.php
+        exit 1
+    fi
+
+    php composer-setup.php
+    rm composer-setup.php
+
+    # Use options because we are running as root, see https://getcomposer.org/root.
+    php composer.phar install --no-plugins --no-scripts
+
+fi
+
+rm /usr/local/vufind/local/httpd-vufind.conf;
+
+if [ "$LOCAL_SOLR" = "true" ]; then
+    echo "Setting up local Solr-Instance.";
+
+    cp /usr/local/vufind-configs/httpd-vufind.conf /usr/local/vufind/local/httpd-vufind.conf;
+    ln -s /usr/local/vufind/local/httpd-vufind.conf /etc/apache2/conf-enabled/vufind.conf
+    export SOLR_ADDITIONAL_START_OPTIONS="-force" # starting as root gets blocked without -force option
+
+    if [ "$LOCAL_SOLR_USE_SAMPLE_DATA" = "true" ]; then
+
+        echo "Using sample data, deleting existing index."
+        rm -rf /usr/local/vufind/solr/vufind/biblio/index
+        /usr/local/vufind/solr.sh start SOLR_ADDITIONAL_START_OPTIONS "-force"
+        # Apache  has to be running for import (provides redirect proxy)
+        service apache2 start
+        /import/sample_import.sh
+        service apache2 stop
+
+        /usr/local/vufind/solr.sh stop
+        echo "Done importing sample data."
+    fi
+
+    /usr/local/vufind/solr.sh start SOLR_ADDITIONAL_START_OPTIONS "-force"
+
+else
+    # Proxy Solr queries to one of the development servers (no local Solr used in this setup)...
+    echo "Using proxied Solr-Instance (redirecting search to 195.37.32.11).";
+    cp /usr/local/vufind-configs/httpd-vufind-dev-local.conf /usr/local/vufind/local/httpd-vufind.conf;
+    ln -s /usr/local/vufind/local/httpd-vufind.conf /etc/apache2/conf-enabled/vufind.conf
+fi
+
+
+echo "Starting apache2..."
+/usr/sbin/apache2ctl -DFOREGROUND
---
+++ sample-data/sample_import.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Importing sample data..."
+/usr/local/vufind/local/import/import.sh /import/*.mrc
+echo "Done."
