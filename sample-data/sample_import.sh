3f46e35de28b12add80efdd689a97d3e1874a2e7
Simon Hohl <mail@dersmon.net>
2019-12-03 09:29:18 +0100

    Create temporary directory for sample files
    Done to reduce warnings/errors. Otherwise VuFind tries to import the
    markdown and shell scripts.

--- sample-data/sample_import.sh
+++ sample-data/sample_import.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+mkdir /tmp/import_files
+cp /import/*.mrc /tmp/import_files
+cp /import/*.marcxml /tmp/import_files
+cp /import/*.xml /tmp/import_files
+
 echo "Importing sample data..."
-/usr/local/vufind/local/import/import.sh /import/*.mrc
+/usr/local/vufind/local/import/import.sh /tmp/import_files/
 echo "Done."
