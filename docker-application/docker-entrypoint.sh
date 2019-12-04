dainst/vufind-docker

commit c161915c0441bfd6f9347df2e6c00e5247b65819

Remove pre-existing symbolic links for Apache configurations

docker-application/docker-entrypoint.sh
@@ -38,7 +38,6 @@ if [ "$LOCAL_SOLR" = "true" ]; then
     echo "Setting up local Solr-Instance.";
 
     cp /usr/local/vufind-configs/httpd-vufind.conf /usr/local/vufind/local/httpd-vufind.conf;
-    ln -s /usr/local/vufind/local/httpd-vufind.conf /etc/apache2/conf-enabled/vufind.conf
     export SOLR_ADDITIONAL_START_OPTIONS="-force" # starting as root gets blocked without -force option
 
     if [ "$LOCAL_SOLR_USE_SAMPLE_DATA" = "true" ]; then
@@ -61,9 +60,12 @@ else
     # Proxy Solr queries to one of the development servers (no local Solr used in this setup)...
     echo "Using proxied Solr-Instance (redirecting search to 195.37.32.11).";
     cp /usr/local/vufind-configs/httpd-vufind-dev-local.conf /usr/local/vufind/local/httpd-vufind.conf;
-    ln -s /usr/local/vufind/local/httpd-vufind.conf /etc/apache2/conf-enabled/vufind.conf
 fi
 
+if [[ -f  /etc/apache2/conf-enabled/vufind.conf ]]; then
+    rm /etc/apache2/conf-enabled/vufind.conf
+fi
+ln -s /usr/local/vufind/local/httpd-vufind.conf /etc/apache2/conf-enabled/vufind.conf
 
 echo "Starting apache2..."
 /usr/sbin/apache2ctl -DFOREGROUND

commit 71b92db33f08c68b60564fb6190f8b262d9c19fc

Create symbolic links before trying to index solr
This was a bug introduced in https://github.com/dainst/vufind-docker/commit/c161915c0441bfd6f9347df2e6c00e5247b65819.

docker-application/docker-entrypoint.sh
@@ -38,6 +38,12 @@ if [ "$LOCAL_SOLR" = "true" ]; then
     echo "Setting up local Solr-Instance.";
 
     cp /usr/local/vufind-configs/httpd-vufind.conf /usr/local/vufind/local/httpd-vufind.conf;
+
+    if [[ -f  /etc/apache2/conf-enabled/vufind.conf ]]; then
+        rm /etc/apache2/conf-enabled/vufind.conf
+    fi
+    ln -s /usr/local/vufind/local/httpd-vufind.conf /etc/apache2/conf-enabled/vufind.conf
+
     export SOLR_ADDITIONAL_START_OPTIONS="-force" # starting as root gets blocked without -force option
 
     if [ "$LOCAL_SOLR_USE_SAMPLE_DATA" = "true" ]; then
@@ -60,12 +66,13 @@ else
     # Proxy Solr queries to one of the development servers (no local Solr used in this setup)...
     echo "Using proxied Solr-Instance (redirecting search to 195.37.32.11).";
     cp /usr/local/vufind-configs/httpd-vufind-dev-local.conf /usr/local/vufind/local/httpd-vufind.conf;
+
+    if [[ -f  /etc/apache2/conf-enabled/vufind.conf ]]; then
+        rm /etc/apache2/conf-enabled/vufind.conf
+    fi
+    ln -s /usr/local/vufind/local/httpd-vufind.conf /etc/apache2/conf-enabled/vufind.conf
 fi
 
-if [[ -f  /etc/apache2/conf-enabled/vufind.conf ]]; then
-    rm /etc/apache2/conf-enabled/vufind.conf
-fi
-ln -s /usr/local/vufind/local/httpd-vufind.conf /etc/apache2/conf-enabled/vufind.conf
 
 echo "Starting apache2..."
 /usr/sbin/apache2ctl -DFOREGROUND