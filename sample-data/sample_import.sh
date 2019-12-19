9fadc4e7806120c7c8bfb860fadd6198c6d1328d
Simon Hohl <simon.hohl@dainst.org>
2019-12-19 09:18:38 +0100

    Make sure all folders for sample data are created

--- sample-data/sample_import.sh
+++ sample-data/sample_import.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-mkdir /tmp/import_files
+mkdir -p /tmp/import_files/bibliographic
 cp /import/bibliographic/*.mrc /tmp/import_files/bibliographic
 cp /import/bibliographic/*.marcxml /tmp/import_files/bibliographic
 cp /import/bibliographic/*.xml /tmp/import_files/bibliographic
 
 echo "Importing bibliographic sample data..."
-/usr/local/vufind/local/import/import.sh /tmp/import_files/bibliographic
+/usr/local/vufind/local/import/import.sh /tmp/import_files/bibliographic/
 echo "Done."
 
+
+mkdir -p /tmp/import_files/authority
 cp /import/authority/*.mrc /tmp/import_files/authority
 cp /import/authority/*.marcxml /tmp/import_files/authority
 cp /import/authority/*.xml /tmp/import_files/authority
 
 echo "Importing authority sample data..."
-/usr/local/vufind/local/import/import-auth.sh /tmp/import_files/authority
+/usr/local/vufind/local/import/import-auth.sh /tmp/import_files/authority/
 echo "Done."
