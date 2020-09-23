8f0ae6b3d7aaaba5cd17fd6754cadf0833889231
Simon Hohl <simon.hohl@dainst.org>
2020-09-23 20:21:15 +0200

    Use new preprocessing script for initial import

--- sample-data/sample_import.sh
+++ sample-data/sample_import.sh
@@ -3,7 +3,8 @@
 mkdir -p /tmp/import_files/bibliographic
 cp /import/bibliographic/*.mrc /tmp/import_files/bibliographic
 cp /import/bibliographic/*.marcxml /tmp/import_files/bibliographic
-cp /import/bibliographic/*.xml /tmp/import_files/bibliographic
+
+python3 /usr/local/vufind/preprocess-marc.py /import/bibliographic /tmp/import_files/bibliographic
 
 echo "Importing bibliographic sample data..."
 /usr/local/vufind/local/import/import.sh /tmp/import_files/bibliographic/
